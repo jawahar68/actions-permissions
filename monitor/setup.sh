cea518e3414eec65eb8aa85d0ee8bd9eb61bc1b1
Jaroslav Lobačevski <jarlob@github.com>
2023-06-29 13:58:02 +0200

    Update setup.sh

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -106,8 +106,9 @@ elif [ "$RUNNER_OS" = "Linux" ]; then
 
   # install mitmproxy certificate as CA
   sudo mkdir /usr/local/share/ca-certificates/extra
-  sudo openssl x509 -in /home/mitmproxyuser/.mitmproxy/mitmproxy-ca-cert.pem -inform PEM -out ~/mitmproxy-ca-cert.crt
-  sudo sh -c "cat ~/mitmproxy-ca-cert.crt >> /etc/ssl/certs/ca-certificates.crt"
+  sudo openssl x509 -in /home/mitmproxyuser/.mitmproxy/mitmproxy-ca-cert.pem -inform PEM -out /tmp/mitmproxy-ca-cert.crt
+  sudo sh -c "cat /tmp/mitmproxy-ca-cert.crt >> /etc/ssl/certs/ca-certificates.crt"
+  rm /tmp/mitmproxy-ca-cert.crt
   sudo dpkg-reconfigure -p critical ca-certificates
   sudo update-ca-certificates
 
